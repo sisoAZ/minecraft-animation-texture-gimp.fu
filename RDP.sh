05f0e382d3c1f9af5a8553271b9f1d5a4af7fbc6
sisoAZ <50191988+sisoAZ@users.noreply.github.com>
2021-05-10 14:44:30 +0900

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m SISO
-sudo adduser SISO sudo
-echo 'SISO:GH' | sudo chpasswd
+sudo useradd -m SISOAZ
+sudo adduser SISOAZ sudo
+echo 'SISOAZ:yukino' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser SISO chrome-remote-desktop
+sudo adduser SISOAZ chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - SISO -c """$CRP"""
+su - SISOAZ -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
